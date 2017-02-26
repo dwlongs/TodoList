f40e41e646e5deae8b0d4fda34de8e843c130540
dwlongs <dwlongs@gmail.com>
2017-02-26 11:04:45 +0800

    add mongoid and rspec

--- todolist/config/application.rb
+++ todolist/config/application.rb
@@ -2,9 +2,17 @@ require_relative 'boot'
 
 require 'rails/all'
 
+env_vars = YAML.load_file('config/env')
+ENV['ENV_NAME'] = env_vars['environment_name']
+ENV['RAILS_ENV'] ||= env_vars['rails_env']
+
+env = ENV['ENV_NAME'].presence || 'development'
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+
+TODOLIST_CONFIG = File.exist?('/etc/todolist/config.yml') ? YAML.load_file('/etc/todolist/config.yml') : YAML.load_file('config/config.yml')
 
 module Todolist
   class Application < Rails::Application
